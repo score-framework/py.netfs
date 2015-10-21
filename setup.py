49447e782d74b989b095fac62e9dd8864eaac5d7
Necdet Can Atesman <c.atesman@gmx.net>
2015-10-21 12:00:14 +0200

    Release: 0.3.8

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open(os.path.join(here, 'README.rst')) as f:
 
 setup(
     name='score.netfs',
-    version='0.3.7',
+    version='0.3.8',
     description='Distributed file storage of The SCORE Framework',
     long_description=README,
     author='strg.at',
