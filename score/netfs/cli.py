f1c6493a43f91a59c1a37e54f12d897fee7072af
Necdet Can Atesman <c.atesman@gmx.net>
2015-11-17 17:15:04 +0100

    CLI can now start using config files
    There are now two separate commands for the command line (serve-conf,
    proxy-conf), enabling starting the servers using config files, which are
    parsed by score.init.parse_config_file()

--- score/netfs/cli.py
+++ score/netfs/cli.py
@@ -29,6 +29,7 @@ import logging
 import logging.config
 import score.netfs as netfs
 import score.init
+import os.path
 
 
 log = logging.getLogger(__name__)
@@ -38,7 +39,7 @@ def init_logging(logconf):
     if logconf is None:
         return
     conf = score.init.config.parse_config_file(logconf)
-    logging.config.fileConfig(conf)
+    logging.config.fileConfig(conf, disable_existing_loggers=False)
 
 
 @click.group()
@@ -53,13 +54,78 @@ def main():
 @click.option('-p', '--port', default=14000)
 @click.option('-l', '--logconf',
               type=click.Path(file_okay=True, dir_okay=False))
-@click.argument('root', type=click.Path(file_okay=False, dir_okay=True))
-def serve(root, host, port, logconf=None):
+@click.argument('folder', type=click.Path(file_okay=False, dir_okay=True))
+def serve(folder, host, port, logconf=None):
     init_logging(logconf)
     from tornado.ioloop import IOLoop
     from .server import StorageServer
     try:
-        server = StorageServer(root)
+        server = StorageServer(folder)
+        server.listen(port, address=host)
+        IOLoop.instance().start()
+        IOLoop.instance().close()
+    except Exception as e:
+        log.exception(e)
+
+
+def read_server_conf(section):
+    try:
+        folder = section['folder']
+    except KeyError:
+        raise click.ClickException('No folder configuration found')
+    try:
+        host = section['host']
+    except KeyError:
+        host = '0.0.0.0'
+    try:
+        port = int(section['port'])
+    except KeyError:
+        port = 14000
+    except ValueError:
+        raise click.ClickException('Configured port could not be parsed')
+    return folder, host, port
+
+
+@main.command('serve-conf')
+@click.argument('conf', type=click.Path(file_okay=True, dir_okay=False))
+@click.argument('name', required=False)
+def serve_conf(conf, name=None):
+    # NOTE: the \b in the following string causes click to print a paragraph
+    # verbatim, i.e. without rewrapping its content.
+    """
+    Start server with config file.
+
+    The configuration file must either contain a section [server] with the
+    following keys:
+
+    \b
+    - `host` (default: 0.0.0.0)
+    - `port` (default: 14000)
+    - `folder` (required, no default).
+
+    If an optional NAME is provided, the application will instead look into the
+    section [server.NAME], but the expected configuration format does not
+    change.
+    """
+    conf = score.init.parse_config_file(conf)
+    if 'loggers' in conf:
+        logging.config.fileConfig(conf, disable_existing_loggers=False)
+    section_name = 'server'
+    if name:
+        section_name = 'server-%s' % name
+    try:
+        section = conf[section_name]
+    except KeyError:
+        raise click.ClickException('Section "%s" not found in configuration'
+                                   % section_name)
+    folder, host, port = read_server_conf(section)
+    if not os.path.isdir(folder):
+        raise click.ClickException('Configured folder (%s) does not exist'
+                                   % folder)
+    from tornado.ioloop import IOLoop
+    from .server import StorageServer
+    try:
+        server = StorageServer(folder)
         server.listen(port, address=host)
         IOLoop.instance().start()
         IOLoop.instance().close()
@@ -83,6 +149,89 @@ def proxy(host, port, backend, logconf=None):
         b[1] = int(b[1])
         backends.append(b)
     try:
+        server = ProxyServer(backends)
+        server.listen(port, address=host)
+        IOLoop.instance().start()
+        IOLoop.instance().close()
+    except Exception as e:
+        log.exception(e)
+
+
+def read_proxy_conf(section):
+    host = '0.0.0.0'
+    port = 14000
+    backends = None
+    try:
+        host = section['host']
+    except KeyError:
+        pass
+    try:
+        port = int(section['port'])
+    except KeyError:
+        port = 14000
+    except ValueError:
+        raise click.ClickException('Configured port (%s) could not be parsed'
+                                   % section['port'])
+    try:
+        backend_list = score.init.parse_list(section['backends'])
+    except KeyError:
+        pass
+    else:
+        backends = []
+        for b in backend_list:
+            b = b.split(':')
+            b[1] = int(b[1])
+            backends.append(b)
+    return host, port, backends
+
+
+@main.command('proxy-conf')
+@click.argument('conf', type=click.Path(file_okay=True, dir_okay=False))
+def proxy_conf(conf, name=None):
+    """
+    Start proxy with config file.
+
+    The configuration file may contain a section [proxy] with the following
+    keys:
+
+    \b
+    - `host` (default: 0.0.0.0)
+    - `port` (default: 14000)
+    - `backends` (default: see below).
+
+    If no such section is found or the section contains no backends definition,
+    the configuration will be done using all other sections in the file, parsing
+    all [server] and [server.NAME] sections as described in the command
+    serve-conf.
+    """
+    conf = score.init.parse_config_file(conf)
+    if 'loggers' in conf:
+        logging.config.fileConfig(conf, disable_existing_loggers=False)
+    if 'proxy' in conf:
+        host, port, backends = read_proxy_conf(conf['proxy'])
+        if backends is None:
+            log.debug('No backends configured, browsing all sections')
+    else:
+        log.debug('No proxy config section, falling back to defaults')
+        host = '0.0.0.0'
+        port = 14000
+        backends = None
+    if backends is None:
+        backends = []
+        for section in conf:
+            if section == 'server' or section.startswith('server-'):
+                folder, host, port = read_server_conf(conf[section])
+                if (host, port) in backends:
+                    log.debug('Ignoring duplicate backend %s:%d' % (host, port))
+                    continue
+                log.debug('Adding backend %s:%d' % (host, port))
+                backends.append((host, port))
+    if not backends:
+        raise click.ClickException('No backends configured')
+    from tornado.ioloop import IOLoop
+    from .proxy import ProxyServer
+    try:
+        log.debug('Starting proxy at %s:%d' % (host, port))
         server = ProxyServer(backends)
         server.listen(port, address=host)
         IOLoop.instance().start()
